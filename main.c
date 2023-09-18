100ab4e39e150d2798b1799e508013340da17cbb
el glioui <elgliouiyoucode@gmail.com>
2023-09-18 10:54:31 +0200

    challeng 1

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*Déclarer un tableau d'entiers de 10 éléments et l'initialiser avec les nombres 1 à 10.
+
+Afficher le tableau en séparant les valeurs par des virgules.*/
+
+
+
+int i;
+int t[10]={1,2,3,4,5,6,7,8,9,10};
+    printf("*******challenge 1 **********\n");
+    for(i=0;i<10;i++){
+      printf("%d /*",t[i]);
+
+
+    }
+
+
+
+
+
+    return 0;
+}
